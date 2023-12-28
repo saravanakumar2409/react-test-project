6b82b81e302ffda637a3ecfadb4dbdcd1bc15180
saravanakumar2409 <153535484+saravanakumar2409@users.noreply.github.com>
2023-12-28 18:42:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Saravana React Project
+          Saravana's React Project
         </p>
         <a
           className="App-link"
